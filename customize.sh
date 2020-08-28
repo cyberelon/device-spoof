cdc6d99a043146827b784ebc4a6250e530a3a013
Cyberelon <70330687+cyberelon007@users.noreply.github.com>
2020-08-28 21:40:21 +0530

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,16 @@
+##########################################################################################
+# NhssssssdmssydNNNNNmyo+osdNNNhssNNNmsshN
+# N:  `...sd   yNNNNo   .`  :mN+  :NNy  /N
+# N:  -NNNNd   yNNNh   hNm`  oN+   +Ny  /N
+# N:  -NNNNd   yNNNs   mNN.  /N+    sy  /N
+# N:   ``sNd   yNNNs   mNN.  :N+  .  +  /N
+# N:  .yymNd   yNNNs   mNN.  :N+  o:    /N
+# N:  -NNNNd   yNNNs   mNN.  /N+  om.   /N
+# N:  .dddmd   oddmd`  ody   yN+  oNd`  /N
+# N:      od      -Nh:     -yNN+  oNNh  /N
+# NNNNNNNNNNNNNNNNNNNNNdddmNNNNNNNNNNNNNNN  Name (codename) - Date
+##########################################################################################
+
+#Print welcome message
+ui_print "- Installing, Name - Date"
+ui_print "- By Elon Musk, Telegram: @eloncyborg"
